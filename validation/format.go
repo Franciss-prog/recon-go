abfe7962b56f81622c608292b1fdf7d87be918cc
Franciss-prog <francisgil.prog@gmail.com>
2026-03-17 20:48:54 +0800

    fix(format.go): change the input params to link so that the net/url package can access flawlessly

--- validation/format.go
+++ validation/format.go
@@ -6,21 +6,24 @@ import (
 	"strings"
 )
 
-func FormatUrl(url string) bool {
+func FormatUrl(link string) bool {
 
 	// check if the length of the link is greater than 0
-	if len(url) == 0 || len(url) >= 255 {
+	if len(link) == 0 || len(link) >= 255 {
 		fmt.Println("Please enter a valid link.")
 		return false
 	}
 
-	// check the scheme
-	if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
-		return false
+	// check the scheme of the url
+	if !strings.HasPrefix(link, "http://") && !strings.HasPrefix(link, "https://") {
+		// this condition is just incase cuz we wont know if the user will enter http or https
+		link = "http://" + link
 	}
+
 	// check the url if the url is valid
-	_, err := url.ParseRequestURI(url)
+	_, err := url.ParseRequestURI(link)
 	if err != nil {
+		fmt.Errorf("Invalid URL: %v", err)
 		return false
 	}
 	return true
--- validation/validate.go
+++ validation/validate.go
@@ -3,7 +3,6 @@ package validation
 import "strings"
 
 func ValidateLink(url string) bool {
-	url = strings.TrimSpace(url)
 
 	return true
 }
